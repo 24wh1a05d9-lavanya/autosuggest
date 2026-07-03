573f777e84f0290f02a3dcdc3f672b7a372b90a1
24wh1a05d9-lavanya <24wh1a05d9@bvrithyderabad.edu.in>
2026-07-03 14:04:08 +0530

    update port

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const app = express()
-const PORT = 3002;
-//change this
 const PORT = process.env.PORT || 3002;
 // web server : 
 app.use(express.static("frontend"))
